452783433b31e8183e35f14090ed0b2d1663073f
Lena Hou <lenahou2013@gmail.com>
2021-03-14 19:36:39 -0400

    refactor

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -26,22 +26,4 @@ class Pokemon
         end
       end    
 
-    def self.display_pokemon
-        self.all.each do |pokemon|
-            puts "#{pokemon.name.upcase}".colorize(:magenta)
-            puts "  height:".colorize(:light_blue) + " #{pokemon.height}"
-            puts "  weight:".colorize(:light_blue) + " #{pokemon.weight}"
-            puts "  id:".colorize(:light_blue) + " #{pokemon.id}"
-            puts "  order:".colorize(:light_blue) + " #{pokemon.order}"
-            puts "  base_experience:".colorize(:light_blue) + " #{pokemon.base_experience}"
-            puts "  moves:".colorize(:light_blue) + " #{pokemon.moves.join(", ")}"
-            puts "  abilities:".colorize(:light_blue) + " #{pokemon.abilities.join(", ")}"
-            puts "  forms:".colorize(:light_blue) + " #{pokemon.forms.join(", ")}"
-            puts "  held_items:".colorize(:light_blue) + " #{pokemon.held_items.join(", ")}"
-            puts "  location_area_encounters:".colorize(:light_blue) + " #{pokemon.location_area_encounters}"
-            puts "-------------------------------------------------------------------------------".colorize(:green)
-    end
-end
-
-
 end
